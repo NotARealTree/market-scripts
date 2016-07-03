661d92f9444a9c471eecedf62e785d64d87226a0
Francis Screene <francis.screene@gmail.com>
2016-07-03 15:57:19 +0100

    Changed faulty script

--- createDB.js
+++ createDB.js
@@ -7,9 +7,8 @@ var MongoClient = require('mongodb').MongoClient
 
 var url = 'mongodb://localhost:27017/tyche';
 MongoClient.connect(url, function(err, db) {
-    db.drop();
-
     var killmails = db.collection("killmails");
+    killmails.drop();
     killmails.createIndex({"solarSystem.id": 1});
     killmails.createIndex({"killTime": 1});
 
